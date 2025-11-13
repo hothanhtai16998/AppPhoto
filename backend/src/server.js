9124fb50dff178bc8c5257a7b4a50484e5d9f6d9
hothanhtai16998 <appstoreusa998@gmail.com>
2025-11-13 22:36:15 +0700

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import { env } from './libs/env.js';
 import { CONNECT_DB } from './configs/db.js';
-
+import path from 'path';
 const app = express();
 
-const PORT = env.PORT || 5001;
 app.use(express.json());
+const PORT = env.PORT || 5001;
+
+const __dirname = path.resolve();
+if(env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../fronted/dist")));
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, '../fronted/dist/index.html'));
+    });
+}
+
+
 
 
 const START_DB = async () => {
