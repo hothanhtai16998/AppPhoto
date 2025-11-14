c70e856761b53df4fe251d626ccf7af57c502988
hothanhtai16998 <appstoreusa998@gmail.com>
2025-11-14 09:44:45 +0700

    add signin form

--- frontend/src/components/auth/signin-form.tsx
+++ frontend/src/components/auth/signin-form.tsx
@@ -1,95 +1,106 @@
 import { cn } from "../../lib/utils"
-import { Button } from "@/components/ui/button"
+
 import { Card, CardContent } from "@/components/ui/card"
-import {
-    Field,
-    FieldDescription,
-    FieldGroup,
-    FieldLabel,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
 
 import { Link } from "react-router"
+import { Label } from "../ui/label"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
 
+import { zodResolver } from "@hookform/resolvers/zod"
 
+const signInSchema = z.object({
+    username: z.string().min(6, { message: "Tên đăng nhập phải có ít nhất 3 ký tự." }),
+    password: z.string().min(8, { message: "Mật khẩu phải có ít nhất 8 ký tự." }),
+});
 
-// const SigninSchema = z.object({
-//   username: z.string().min(6, { message: "Tên đăng nhập không được để trống." }),
-//   password: z.string().min(8, { message: "Mật khẩu không được để trống." }),
-// })
-
-// type signInFormValue = z.infer<typeof SigninSchema>
+type SignInFormValue = z.infer<typeof signInSchema>
 
 export function SigninForm({
     className,
     ...props
 }: React.ComponentProps<"div">) {
+    // const { signUp } = useAuthStore();
 
-    //     const { signIn } = useAuthStore();
-    //     const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    //     const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<signInFormValue>({
-    //         resolver: zodResolver(SigninSchema),
-    //     })
-    //     const onSubmit = async (data: signInFormValue) => {
-    //         const { username, password } = data;
-    //         await signIn(username, password);
-    //         navigate("/");
-    //     }
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<SignInFormValue>({
+        resolver: zodResolver(signInSchema),
+    })
+
+    const onSubmit = async (data: SignInFormValue) => {
+        //gọi backend để xử lý
+        // const validatedData = data as SignupFormValue;
+        // const { firstname, lastname, username, email, password } = validatedData;
+        // await signUp(username, password, email, firstname, lastname)
+        // navigate("/signin");
+    }
 
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
-            <Card className="overflow-hidden p-0">
+            <Card className="overflow-hidden p-0 border-border">
                 <CardContent className="grid p-0 md:grid-cols-2">
-                    <form className="p-6 md:p-8">
-                        <FieldGroup>
-                            <div className="flex flex-col items-center gap-2 text-center">
-                                <h3 className="text-2xl font-bold">Đăng nhập PhotoSharing</h3>
-                                <p className="text-muted-foreground text-sm text-balance">
-                                    Chào mừng bạn! Hãy đăng nhập để bắt đầu!
+                    <form className="p-6 md:p-8" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="flex flex-col gap-6">
+                            {/* header - logo */}
+                            <div className="flex flex-col items-center text-center-gap-2">
+                                <Link to='/' className="mx-auto block w-fit text-center">
+                                    <img src="/logo" alt="logo" />
+                                </Link>
+                                <h1 className="text-2xl font-bold">Chào mừng quay lại</h1>
+                                <p className="text-muted-foreground text-balance">
+                                    Đăng nhập tài khoản PhotoApp của bạn
                                 </p>
                             </div>
 
-                            <Field>
-                                <FieldLabel htmlFor="username">Tên đăng nhập</FieldLabel>
-                                <Input
-                                    id="username"
-                                    type="string"
-                                    placeholder=""
+                            {/** Tên tài khoản */}
+                            <div className="flex flex-col gap-3">
+                                <Label htmlFor='username' className="block text-sm" >
+                                    Tên đăng nhập
+                                </Label>
+                                <Input type="text" id='username' {...register('username')} />
+                                {errors.username && <p className="text-destructive text-sm">
+                                    {errors.username.message}</p>}
 
-                                />
+                            </div>
 
-                            </Field>
+                            {/*  Mật khẩu */}
+                            <div className="flex flex-col gap-3">
+                                <Label htmlFor='password' className="block text-sm" >
+                                    Mật khẩu
+                                </Label>
+                                <Input type="password" id='password' {...register('password')} />
+                                {errors.password && <p className="text-destructive text-sm">
+                                    {errors.password.message}</p>}
 
-                            <Field>
-                                <FieldLabel htmlFor="password">Mật Khẩu</FieldLabel>
-                                <Input id="password" type="password" />
-
-                            </Field>
-
-
-                            <Field>
-                                <Button type="submit" >Đăng nhập</Button>
-                            </Field>
-
-                            <FieldDescription className="text-center">
-                                Chưa có tài khoản? <Link to="/signup" className="underline underline-offset-4">Đăng ký</Link>
-                            </FieldDescription>
-                        </FieldGroup>
+                            </div>
+                            {/*  Đăng nhập */}
+                            <Button type="submit" className="w-full" disabled={isSubmitting}>
+                                Đăng nhập
+                            </Button>
+                            <div className="text-center text-sm">
+                                Chưa có tài khoản? {" "}
+                                <Link to='/signup' className="underline underline-offset-4">
+                                    Đăng ky
+                                </Link>
+                            </div>
+                        </div>
                     </form>
                     <div className="bg-muted relative hidden md:block">
                         <img
                             src="/placeholderSignin.jpeg"
                             alt="Image"
-                            className="absolute inset-0 h-full w-full object-cover"
+                            className="absolute h-full object-cover"
                         />
                     </div>
                 </CardContent>
             </Card>
-            <div className="text-xs text-balance px-6 text-center">
+            <div className="text-xs text-balance px-6 text-center *:[a]:hover:text-primary text-muted-foreground *:[a]:underline *:[a]:underline-offset-4">
                 Bằng cách tiếp tục, bạn đồng ý với <a href="#">điều khoản dịch vụ</a>{" "}
                 và <Link to="#">chính sách bảo mật</Link> của chúng tôi.
             </div>
-        </div>
+        </div >
     )
 }
